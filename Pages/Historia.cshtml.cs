37c429ed17da91ac5a2c1f96c1f8d1ab0efab2de
Gerson Soares Vieira <Gerson Soares Vieira@DESKTOP-FU7KN45>
2022-12-07 21:16:22 -0300

    Adicionar arquivos de projeto.

---
+++ Pages/Historia.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MeuPortfolio.Pages
+{
+    public class HistoriaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
