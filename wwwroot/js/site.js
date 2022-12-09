da3e46a409a119fc80cd7c5838e66d517e21a88c
Gerson Soares <gerson_soares@outlook.com.br>
2022-12-09 09:19:47 -0300

    atualizacao botao oi

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+
+    $(document).on("click", "#btnOi", function () {
+        alert("oi");
+    });
+
+});
